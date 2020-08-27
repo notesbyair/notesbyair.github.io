3176179dc7a2e800d559ef92c723b9ab8f342159
Ajay Ramesh <ajaycs14@gmail.com>
2020-08-27 09:48:32 -0500

    rewrite

--- assets/js/search-worker-9.0.3.js
+++ assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-08-27T13:31:05+00:00';
+const INDEX_KEY = 'index--2020-08-27T14:47:38+00:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
